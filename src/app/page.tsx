97f695d0561562e7d63db09adc48b7073d2431b8
Hamza-GHub <1aladelyham@hdsb.ca>
2025-05-07 01:50:54 -0400

    Testing
    write ur name below to make sure everyone can pull and push

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl">Write you name below</h1>
+      <p className="text-5xl">Hamza</p>
+    </div>
+  );
+}
